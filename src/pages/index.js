3781fc8bfc17a551e08eac4019a5142efe1ea26d
marifer <mafevito@hotmail.com>
2018-08-28 18:31:12 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello! </h1>
+    <p>I'm a web full stack developer, self-taught, in continuous transformation, sharing her journey as a woman in technology. 👩🏽‍💻</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
