fc276d1c7c42c7ffbaa282b6b871e8e985ea438d
marifer <mafevito@hotmail.com>
2018-08-28 19:15:52 +0200

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hello! </h1>
+    <p>My name is Marifer, I'm 18 years old. </p>
     <p>I'm a web full stack developer, self-taught, in continuous transformation, sharing her journey as a woman in technology. 👩🏽‍💻</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
